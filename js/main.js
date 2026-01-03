ad1a3560448bd56c6390ee32390b06ec88f21c8d
Mohamed Ashraf <mohamed21-01148@student.eelu.edu.eg>
2026-01-03 07:55:35 +0200

    Add modal for updating product details and enhance styling

--- js/main.js
+++ js/main.js
@@ -2,232 +2,193 @@ var productNameInput = document.getElementById('productName');
 var productPriceInput = document.getElementById('productPrice');
 var productCategoryInput = document.getElementById('productCategory');
 var productDescInput = document.getElementById('productDescription');
-var productSearchInput = document.getElementById('productSearch');
 var productImageInput = document.getElementById('productImage');
+var productSearchInput = document.getElementById('productSearch');
+
+var uName = document.getElementById('uName');
+var uPrice = document.getElementById('uPrice');
+var uCategory = document.getElementById('uCategory');
+var uDesc = document.getElementById('uDesc');
+var uImage = document.getElementById('uImage');
 
 var productList = [];
+var updateIndex = null;
 
-if(localStorage.getItem('productList') != null){
+if (localStorage.getItem('productList')) {
     productList = JSON.parse(localStorage.getItem('productList'));
-
     displayProucts();
 }
 
-function addProduct(){
+function addProduct() {
+    if (
+        validate(productNameInput, nameRegex, 'nameError') &&
+        validate(productPriceInput, priceRegex, 'priceError') &&
+        validate(productCategoryInput, categoryRegex, 'categoryError') &&
+        validate(productDescInput, descRegex, 'descError')
+    ) {
+        var imageValue = productImageInput.files[0]
+            ? productImageInput.files[0].name
+            : 'portfolio-3.jpg';
 
-    if(validateName() && validatePrice() && validateCategory() && validateDesc()){
-
-        var imageValue = productImageInput.files[0] ? productImageInput.files[0].name : 'portfolio-3.jpg';
-
-        var product = {
-            name : productNameInput.value,
-            price : productPriceInput.value,
-            category : productCategoryInput.value,
-            desc : productDescInput.value,
-            image : `images/${imageValue}`
-        }
-
-        productList.push(product);
+        productList.push({
+            name: productNameInput.value,
+            price: productPriceInput.value,
+            category: productCategoryInput.value,
+            desc: productDescInput.value,
+            image: `images/${imageValue}`
+        });
 
         localStorage.setItem('productList', JSON.stringify(productList));
-
         displayProucts();
-
         clearForm();
 
-        // console.log(productList);
-
-        Swal.fire({
-            icon: "success",
-            title: "Good Job...",
-            text: "Product Added Successfully",
-        });
-
-
+        Swal.fire("Success", "Product Added Successfully", "success");
     }
-
-    else{
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Product Not Added",
-        });
-    }
-
 }
 
-
-function clearForm(){
-    productNameInput.value = "";
-    productPriceInput.value = "";
-    productCategoryInput.value = "";
-    productDescInput.value = "";
-    productImageInput.value = "";
-}
-
-
-function displayProucts(){
+function displayProucts() {
 
     var box = "";
 
-    for(var i = 0 ; i < productList.length ; i++){
-        box+= `
-                <div class="my-card col-md-6 col-lg-4 col-xl-3">
-                    <div class="inner shadow-lg rounded-3 overflow-hidden">
-                        <img class="w-100" src="${productList[i].image}" alt="${productList[i].name}">
+    for (var i = 0; i < productList.length; i++) {
+        box += `
+        <div class="my-card col-md-6 col-lg-4 col-xl-3">
+            <div class="inner shadow-lg rounded-3 overflow-hidden">
 
-                        <div class="p-2">
-                            <span class="badge text-bg-info text-white">Index : ${i}  </span>
-                            <h5>Product Name : ${productList[i].name}</h5>
-                            <p>Price : ${productList[i].price}</p>
-                            <p>Category : ${productList[i].category}</p>
-                            <p>Description : ${productList[i].desc}</p>
-                        </div>
-                        <div class="border-top bg-body-tertiary p-2 d-flex justify-content-center gap-2">
-                            <button class="btn btn-outline-warning">Update <i
-                                class="fa-solid fa-pen-to-square"></i></button>
-                            <button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete <i
-                                class="fa-solid fa-trash"></i></button>
-                        </div>
-                    </div>
+                <img class="w-100" src="${productList[i].image}" alt="${productList[i].name}">
+
+                <div class="p-2">
+                    <span class="badge text-bg-info text-white">
+                        Index : ${i}
+                    </span>
+                    <h5>Product Name : ${productList[i].name}</h5>
+                    <p>Price : ${productList[i].price}</p>
+                    <p>Category : ${productList[i].category}</p>
+                    <p>Description : ${productList[i].desc}</p>
                 </div>
-        `
+
+                <div class="border-top bg-body-tertiary p-2 d-flex justify-content-center gap-2">
+
+                    <button
+                        onclick="openUpdateModal(${i})"
+                        data-bs-toggle="modal"
+                        data-bs-target="#updateModal"
+                        class="btn btn-outline-warning">
+                        Update <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+
+                    <button
+                        onclick="deleteProduct(${i})"
+                        class="btn btn-outline-danger">
+                        Delete <i class="fa-solid fa-trash"></i>
+                    </button>
+
+                </div>
+
+            </div>
+        </div>
+        `;
     }
 
     document.getElementById('rowData').innerHTML = box;
 }
 
 
-function deleteProduct(index){
-    productList.splice(index , 1);
+function deleteProduct(index) {
+    productList.splice(index, 1);
     localStorage.setItem('productList', JSON.stringify(productList));
     displayProucts();
-
 }
 
-
-function searchProduct(){
-    // console.log(productSearchInput.value);
-
-    var searchValue = productSearchInput.value;
-    var box = "";
-
-    for(var i = 0 ; i < productList.length ; i++){
-        if(productList[i].name.toLowerCase().includes(searchValue.toLowerCase())){
-            box+= `
-                <div class="my-card col-md-6 col-lg-4 col-xl-3">
-                    <div class="inner shadow-lg rounded-3 overflow-hidden">
-                        <img class="w-100" src="${productList[i].image}" alt="${productList[i].name}">
-
-                        <div class="p-2">
-                            <span class="badge text-bg-info text-white">Index : ${i}  </span>
-                            <h5>Product Name : ${productList[i].name}</h5>
-                            <p>Price : ${productList[i].price}</p>
-                            <p>Category : ${productList[i].category}</p>
-                            <p>Description : ${productList[i].desc}</p>
-                        </div>
-                        <div class="border-top bg-body-tertiary p-2 d-flex justify-content-center gap-2">
-                            <button class="btn btn-outline-warning">Update <i
-                                class="fa-solid fa-pen-to-square"></i></button>
-                            <button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete <i
-                                class="fa-solid fa-trash"></i></button>
-                        </div>
+function searchProduct() {
+    var term = productSearchInput.value.toLowerCase();
+    document.getElementById('rowData').innerHTML = productList
+        .filter(p => p.name.toLowerCase().includes(term))
+        .map(p => `
+            <div class="col-md-3">
+                <div class="card shadow">
+                    <img src="${p.image}" class="card-img-top">
+                    <div class="card-body">
+                        <h5>${p.name}</h5>
+                        <p>${p.price}</p>
                     </div>
                 </div>
-        `
-        }
-    }
-
-    document.getElementById('rowData').innerHTML = box;
+            </div>
+        `).join('');
 }
 
+function openUpdateModal(index) {
+    updateIndex = index;
 
+    uName.value = productList[index].name;
+    uPrice.value = productList[index].price;
+    uCategory.value = productList[index].category;
+    uDesc.value = productList[index].desc;
 
+    resetModalValidation();
+}
 
-function validateName(){
-    var nameRegex = /^[A-Z][a-z]{2,15}$/;
+function updateProduct() {
 
-    if(nameRegex.test(productNameInput.value)){
-        productNameInput.classList.add('is-valid');
-        productNameInput.classList.remove('is-invalid');
-        document.getElementById('nameError').classList.add('d-none');
-        return true;
+    if (
+        validate(uName, nameRegex, 'uNameError') &&
+        validate(uPrice, priceRegex, 'uPriceError') &&
+        validate(uCategory, categoryRegex, 'uCategoryError') &&
+        validate(uDesc, descRegex, 'uDescError')
+    ) {
+
+        var imageValue = uImage.files[0]
+            ? uImage.files[0].name
+            : productList[updateIndex].image.split('/')[1];
+
+        productList[updateIndex] = {
+            name: uName.value,
+            price: uPrice.value,
+            category: uCategory.value,
+            desc: uDesc.value,
+            image: `images/${imageValue}`
+        };
+
+        localStorage.setItem('productList', JSON.stringify(productList));
+        displayProucts();
+
+        bootstrap.Modal.getInstance(
+            document.getElementById('updateModal')
+        ).hide();
+
+        Swal.fire("Updated", "Product Updated Successfully", "success");
     }
+}
 
-    else{
-        productNameInput.classList.add('is-invalid');
-        productNameInput.classList.remove('is-valid');
-        document.getElementById('nameError').classList.remove('d-none');
+function clearForm() {
+    [productNameInput, productPriceInput, productCategoryInput, productDescInput]
+        .forEach(input => input.classList.remove('is-valid', 'is-invalid'));
+}
+
+var nameRegex = /^[A-Z][a-z]{2,15}$/;
+var priceRegex = /^[1-9][0-9]{2,8}$/;
+var categoryRegex = /^[A-Z][a-z]{3,15}$/;
+var descRegex = /.*[a-zA-Z].*/;
+
+function validate(input, regex, errorId) {
+    if (regex.test(input.value)) {
+        input.classList.add('is-valid');
+        input.classList.remove('is-invalid');
+        document.getElementById(errorId).classList.add('d-none');
+        return true;
+    } else {
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        document.getElementById(errorId).classList.remove('d-none');
         return false;
     }
 }
 
-
-
-
-function validatePrice(){
-
-    var priceRegex = /^[1-9][0-9]{2,8}$/;
-
-    if (priceRegex.test(productPriceInput.value)) {
-        productPriceInput.classList.add('is-valid');
-        productPriceInput.classList.remove('is-invalid');
-        document.getElementById('priceError').classList.add('d-none');
-        return true;
-    }
-
-    else{
-        productPriceInput.classList.add('is-invalid');
-        productPriceInput.classList.remove('is-valid');
-        document.getElementById('priceError').classList.remove('d-none');
-        return false;
-    }
+function resetModalValidation() {
+    [uName, uPrice, uCategory, uDesc]
+        .forEach(input => input.classList.remove('is-valid', 'is-invalid'));
 }
 
-
-
-function validateCategory(){
-
-    var categoryRegex = /^[A-Z][a-z]{3,15}$/;
-
-    if (categoryRegex.test(productCategoryInput.value)) {
-        productCategoryInput.classList.add('is-valid');
-        productCategoryInput.classList.remove('is-invalid');
-        document.getElementById('categoryError').classList.add('d-none');
-        return true;
-    }
-
-    else{
-        productCategoryInput.classList.add('is-invalid');
-        productCategoryInput.classList.remove('is-valid');
-        document.getElementById('categoryError').classList.remove('d-none');
-        return false;
-    }
-    
-}
-
-
-function validateDesc(){
-
-    // var descRegex = /^(.|\s)*[a-zA-Z]+(.|\s)*$/;
-    var descRegex = /.*[a-zA-Z].*/;
-
-
-    if (descRegex.test(productDescInput.value)) {
-        productDescInput.classList.add('is-valid');
-        productDescInput.classList.remove('is-invalid');
-        document.getElementById('descError').classList.add('d-none');
-        return true;
-    }
-
-    else{
-        productDescInput.classList.add('is-invalid');
-        productDescInput.classList.remove('is-valid');
-        document.getElementById('descError').classList.remove('d-none');
-        return false;
-    }
-
-}
 
 
 
